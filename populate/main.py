d257b43b339de3fe35b337742d26f61595381155
jayteealao <jayteealao@gmail.com>
2021-08-20 16:00:51 +0100

    Added logging capability to the populate lambda function.

--- populate/main.py
+++ populate/main.py
@@ -1,20 +1,36 @@
-from populate.crud.create import populate_tables
-from populate.crud.read import read_by_league
-from populate.database import database, engine, metadata
+import os
 import asyncio
+check = os.environ.get('AWS_REGION', None)
+
+if check == None:
+    import sys
+    sys.path.append("C:/Users/HP/Documents/dev/BetEyeView")
+print(sys.path)
+
+
+from populate.crud.create import populate_tables
+from populate.database import database, engine, metadata
+from populate.utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 async def main():
-    print('Initializing Scratch...')
-    await database.connect()
+    try:
+        await database.connect()
+        logger.info('Database connection established')
+    except Exception as e:
+        logger.error(e)
     async with database.transaction():
-        await populate_tables()
-        test = await read_by_league("Premier League")
-        for match in test:
-            print(match)
+        try:
+            await populate_tables()
+            logger.info('Tables populated')
+        except Exception as e:
+            logger.error(e)
 
 
 def handler(event, context):
+    logger.info('Started function')
     asyncio.run(main())
 
 
